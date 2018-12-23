dfd652f092ab1f012ecd79c91c50cf308b16448a
bozsandris <33602698+bozsandris@users.noreply.github.com>
2018-12-23 22:59:31 +0100

    Fixed minor bug

--- app/src/main/java/com/example/bozsi/progression/MainActivity.java
+++ app/src/main/java/com/example/bozsi/progression/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
--- app/src/main/java/com/example/bozsi/progression/Timer.java
+++ app/src/main/java/com/example/bozsi/progression/Timer.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Timer extends AppCompatActivity {
 
